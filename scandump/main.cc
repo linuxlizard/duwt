c6c01b1088019706c50c00f362b243b6f4c01918
David Poole <dpoole@cradlepoint.com>
2019-03-23 07:52:47 -0600

    rename the Cfg80211 instance to something less confusing

--- scandump/main.cc
+++ scandump/main.cc
@@ -1,3 +1,5 @@
+/* Simple CLI program to retrieve and decode scan results
+ */
 #include <iostream>
 #include <vector>
 
@@ -5,13 +7,13 @@
 
 int main(void)
 {
-	cfg80211::Cfg80211 cfg80211;
+	cfg80211::Cfg80211 iw;
 
 	std::array<uint8_t, 10> buf {};
 	auto ie = cfg80211::IE(255,12, buf.data());
 
 	std::vector<cfg80211::BSS> bss_list;
-	cfg80211.get_scan("wlp1s0", bss_list);
+	iw.get_scan("wlp1s0", bss_list);
 
 	for ( auto&& bss : bss_list ) {
 		std::cout << "found BSS " << bss << " num_ies=" << bss.ie_list.size() << "\n";
