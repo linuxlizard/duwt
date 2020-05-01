bf0e3dd8b5fe61684dbce3a0ebe0d25f68613cb9
David Poole <testcluster@gmail.com>
2020-05-01 17:43:51 -0600

    add files I missed in initial check-in

---
+++ Python/util.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// iw util.c 
+void mac_addr_n2a(char *mac_addr, const unsigned char *arg)
+{
+	int i, l;
+
+	l = 0;
+	for (i = 0; i < ETH_ALEN ; i++) {
+		if (i == 0) {
+			sprintf(mac_addr+l, "%02x", arg[i]);
+			l += 2;
+		} else {
+			sprintf(mac_addr+l, ":%02x", arg[i]);
+			l += 3;
+		}
+	}
+}
+
+
---
+++ Python/util.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+void mac_addr_n2a(char *mac_addr, const unsigned char *arg);
+
+#endif
+
