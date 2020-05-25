32b8e21aad3227c51fdcee84144927cc43037151
David Poole <testcluster@gmail.com>
2020-05-25 14:32:10 -0600

    switch from Poco to plain jsoncpp
    https://github.com/open-source-parsers/jsoncpp
    
    because I can't make up my mind about anything /eyeroll

--- httpserv/bss_json.cc
+++ httpserv/bss_json.cc
@@ -11,26 +11,10 @@
 #include <netlink/genl/ctrl.h>
 #include <linux/nl80211.h>
 
-#include "Poco/Logger.h"
-#include "Poco/ConsoleChannel.h"
-#include "Poco/StreamChannel.h"
-#include "Poco/JSON/Object.h"
-#include "Poco/JSON/JSONException.h"
-#include "Poco/UTF8String.h"
+#include "json/json.h"
 
 #include "bss.h"
 #include "nlnames.h"
-
-using Poco::Logger;
-using Poco::Message;
-
-Logger& logger = Logger::root();
-
-// newer versions of Poco have this
-#ifndef poco_information_f
-#define poco_information_f(logger, fmt, ...) \
-	if ((logger).information()) (logger).information(Poco::format((fmt), __VA_ARGS__), __FILE__, __LINE__); else (void) 0
-#endif
 
 static const uint32_t SCAN_DUMP_COOKIE = 0x64617665;
 
@@ -48,8 +32,6 @@ private:
 
 void parse(std::istream& infile, std::vector<std::unique_ptr<BSSPointer>>& bss_list)
 {
-	logger.debug("parse ");
-
 	if (infile.fail()) {
 		throw std::runtime_error("file open failed");
 	}
@@ -81,7 +63,6 @@ void parse(std::istream& infile, std::vector<std::unique_ptr<BSSPointer>>& bss_l
 			throw std::runtime_error("file format error; invalid size");
 		}
 
-		logger.debug("size=%u cookie=0x%x", size, cookie);
 		uint8_t buf[4096];
 		infile.read((char *)buf, size);
 		if (infile.fail()) {
@@ -104,13 +85,13 @@ void parse(std::istream& infile, std::vector<std::unique_ptr<BSSPointer>>& bss_l
 	}
 }
 
-Poco::JSON::Object ie_to_json(const struct IE* ie)
+Json::Value ie_to_json(const struct IE* ie)
 {
 	// may the gods of RVO smile upon me
-	Poco::JSON::Object obj;
+	Json::Value obj;
 
-	obj.set("id", static_cast<int>(ie->id));
-	obj.set("len", ie->len);
+	obj["id"] =  static_cast<int>(ie->id);
+	obj["len"] = ie->len;
 
 	std::ostringstream ostr;
 //	ostr.str(bytes);
@@ -118,8 +99,7 @@ Poco::JSON::Object ie_to_json(const struct IE* ie)
 	for (int i=0 ; i<ie->len ; i++) {
 		ostr << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(ie->buf[i]);
 	}
-	logger.debug("ostr=%s", ostr.str());
-	obj.set("bytes", ostr.str());
+	obj["bytes"] = ostr.str();
 
 	// TODO
 #if 0
@@ -150,40 +130,38 @@ Poco::JSON::Object ie_to_json(const struct IE* ie)
 	return obj;
 }
 
-Poco::JSON::Object bss_to_json(const struct BSS* bss)
+Json::Value bss_to_json(const struct BSS* bss)
 {
 	// may the gods of RVO smile upon me
-	Poco::JSON::Object obj;
+	Json::Value obj(Json::objectValue);
 
-	obj.set("bssid", bss->bssid_str);
+	obj["bssid"] =  bss->bssid_str;
 
 	// dangerous untrusted input!
 	const struct IE_SSID* ie_ssid = bss_get_ssid(bss);
 	if (ie_ssid) {
 		std::string ssid_unsafe { ie_ssid->ssid, ie_ssid->ssid_len };
-		std::string ssid_esc = Poco::UTF8::escape(ssid_unsafe, true);
-		obj.set("ssid", ssid_esc);
-		logger.information("found ssid=%s", ssid_esc);
+//		std::string ssid_esc = Poco::UTF8::escape(ssid_unsafe, true);
+//		obj["ssid"]  = ssid_esc;
 	}
 	else {
-		obj.set("ssid", "<hidden>");
+		obj["ssid"] = "<hidden>";
 	}
 
-	obj.set("frequency", bss->frequency);
-	obj.set("dBm", bss->signal_strength_mbm / 100.0);
+	obj["frequency"] =  bss->frequency;
+	obj["dBm"] = bss->signal_strength_mbm / 100.0;
 	char str[32] {};
 	bss_get_chan_width_str(bss, str, 32);
-	obj.set("channel_width", std::string(str));
-	obj.set("TSF", bss->tsf);
+	obj["channel_width"] =  std::string(str);
+	obj["TSF"] = bss->tsf;
 
-	Poco::JSON::Array ie_array;
+	Json::Value  ie_array(Json::arrayValue);
 	const struct IE* ie = nullptr;
 	ie_list_for_each_entry(ie, bss->ie_list) {
-//		ie_array.add(static_cast<int>(ie_ptr->id));
-		ie_array.add(ie_to_json(ie));
+		ie_array.append(ie_to_json(ie));
 	}
 
-	obj.set("IE", ie_array);
+	obj["IE"] = ie_array;
 
 	return obj;
 }
@@ -191,22 +169,6 @@ Poco::JSON::Object bss_to_json(const struct BSS* bss)
 int main(void)
 {
 	std::cout << "hello, world\n";
-
-	logger.setChannel(new Poco::StreamChannel(std::cout));
-	logger.setLevel(Message::PRIO_DEBUG);
-	std::cout << "level=" << logger.getLevel() << "\n";
-	logger.debug("This is a Debug message");
-	logger.information("This is an Information message");
-	logger.warning("This is a Warning message");
-	logger.error("This is an Error message");
-
-	poco_warning(logger, "Another warning");
-	poco_information(logger, Poco::format("information with args: %d %d", 42, 54));
-	poco_information_f(logger, "information with args: %d %d", 42, 54);
-
-	Logger::get("ConsoleLogger").error("Another error message");
-
-//	Logger& logger = Logger::create("iw", 
 
 	std::ifstream infile;
 	infile.open("scan-dump.dat", std::ios_base::binary|std::ios_base::in);
@@ -219,14 +181,15 @@ int main(void)
 	for (auto& bss_ptr: bss_list) {
 		const struct BSS* bss = bss_ptr.get()->get();
 
-		Poco::JSON::Object obj = bss_to_json(bss);
-		std::ostringstream str;
-		obj.stringify(str);
-		std::cout << str.str() << "\n";
+		Json::Value obj = bss_to_json(bss);
+//		std::ostringstream str;
+		Json::StreamWriterBuilder builder;
+		builder["indentation"] = "";
+		const std::string json_file = Json::writeString(builder, obj);
+//		obj.stringify(str);
+		std::cout << json_file << "\n";
 //		obj.stringify(std::cout, 5);
 	}
-
-	logger.information("found %z bss", bss_list.size());
 
 	return 0;
 }
