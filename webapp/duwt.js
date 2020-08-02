c750e191cbea1ab5a6fa5510078533ca26312531
David Poole <testcluster@gmail.com>
2020-08-02 09:40:30 -0600

    several updates
    - add a simple index.html to allow me to test/learn js/jquery in a
      simple page
    - small clean-ups to duwt's adminlte page
    - start of class for fetching survey data

--- webapp/duwt.js
+++ webapp/duwt.js
@@ -3,16 +3,81 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-$(document.body).append("<div><h1>Greetings</h1><p>Yoshi here</p></div>")
-
-console.log("hello, world")
-
-$(document).ready(function() {
-	var q = {url:"/api/survey", type:"GET", dataType:"json"}
-	var done = function(json) {
-		console.log(json)
+class Survey {
+	constructor(url){
+		this.url = url;
+		this.survey = [];
 	}
-	$.ajax(q).done(done)
+
+	fetch() {
+		return fetch(this.url)
+			.then(response => { console.log("controller response",response)
+								if (! response.ok || response.headers.get("content-type") != "application/json") {
+									throw new TypeError("get survey failed"); 
+								}
+								return response.json();
+							})
+			.then(json => { console.log("json=",json);
+							var new_survey = $.map(json, (network,idx) => {
+										return {
+											date: new Date(),
+											...network,
+											};
+									});
+							this.survey = new_survey;
+							return Promise.resolve(new_survey);
+						  })
+	}
+
+}
+
+
+// playground for learning wtf I'm doing
+$(document).ready(function() {
+	console.log("hello, world");
+	console.log("survey=",window.survey);
+
+	$.each(window.survey, (idx,network) => console.log(network));
+
+	var new_survey = $.map(window.survey, (network,idx) => {
+				return {
+					savebssid:network.bssid,
+					...network,
+					bssid:`<a href=/bssid/${network.bssid}>${network.bssid}</a>`,
+					};
+			});
+
+	var survey = new Survey("/api/survey");
+	var survey_promise = survey.fetch();
+	console.log("promise=",survey_promise);
+	console.log("survey=",survey.survey);
+	var table = $("#table-duwt");
+	var tablebody = $("#table-duwt > tbody");
+
+	var filltable = function(survey) {
+		console.log("survey complete; survey=", survey); 
+		console.log("table=",table);
+		console.log("this=",this);
+		$.each( survey, function(index,value) {
+			console.log(`${value.bssid} ${value.ssid} ${value.freq}`);
+			// BSSID SSID Frequency
+			tablebody.append(
+					"<tr>" + 
+						`<td>${value.bssid}</td>` +
+						`<td>${value.ssid}</td>` + 
+						`<td>${value.freq}</td>` + 
+					"</tr>");
+				});
+	}
+
+	// assuming I'm running from index.html which has a simple table; update
+	// the table
+	survey_promise.then(filltable.bind(tablebody));
+//	survey_promise.then( survey => { 
+//				console.log("survey complete; survey=", survey); 
+//				console.log("table=",table);
+//			} );
 
 })
+
 
