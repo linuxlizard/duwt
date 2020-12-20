a1ecda1bac7cace8d1594ffa10ba95bad9976407
David Poole <dpoole@cradlepoint.com>
2020-12-20 15:47:42 -0700

    add support for sending files under 'files' subdir
    lots of checks for to prevent relative paths

--- httpserv/poco-httpd.cc
+++ httpserv/poco-httpd.cc
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <thread>
+#include <string>
+#include <optional>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "Poco/Net/HTTPServer.h"
 #include "Poco/Net/HTTPRequestHandler.h"
@@ -19,6 +25,34 @@ using namespace Poco::Util;
 
 mimetypes all_mimetypes;
 
+static std::optional<std::string> make_absolute(std::string path)
+{
+	char abspath[PATH_MAX+1];
+
+	char* p = realpath(path.c_str(), abspath);
+	if (!p) {
+		return std::nullopt;
+	}
+
+	return std::string { abspath };
+}
+
+static bool is_file(std::string& path)
+{
+	struct stat statbuf;
+
+	int ret = stat(path.c_str(), &statbuf);
+	if (ret != 0) {
+		return false;
+	}
+
+	if ( (statbuf.st_mode & S_IFREG) != S_IFREG ) {
+		return false;
+	}
+
+	return true;
+}
+
 class HelloRequestHandler: public HTTPRequestHandler
 {
 	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
@@ -33,26 +67,50 @@ class HelloRequestHandler: public HTTPRequestHandler
 		Poco::Path path {false}; // path will be relative
 		path.pushDirectory("files");
 		path.setFileName(uri);
-		app.logger().information("file=%s", path.toString(Poco::Path::PATH_UNIX));
 
-		std::string ext = path.getExtension();
+		// poco absolute()/makeAbsolute() don't do what I hope it would do
+//		path = path.absolute();
+//		path.makeAbsolute();
+		// so let's do this the posix-y way
+		auto path_absolute = make_absolute(path.toString(Poco::Path::PATH_UNIX));
+		if ( !path_absolute.has_value() ) {
+			app.logger().error("invalid absolute path uri=%s", uri);
+			response.setStatusAndReason(HTTPResponse::HTTP_NOT_FOUND);
+			response.setContentLength(0);
+			response.send();
+			return;
+		}
+
+		std::string path_str = path_absolute.value();
+		app.logger().information("file=%s", path_str);
+
+		// is uri path under current dir?
+		std::string cwd { Poco::Path::current() };
+		app.logger().information("cwd=%s substr=%s", cwd, path_str.substr(0, cwd.size()) );
+
+		if ( path_str.substr(0, cwd.size()) != cwd) {
+			app.logger().error("request for invalid path uri=%s", path_str);
+			response.setStatusAndReason(HTTPResponse::HTTP_NOT_FOUND);
+			response.setContentLength(0);
+			response.send();
+			return;
+		}
+
+		if (!is_file(path_str)) {
+			app.logger().error("uri=%s not a file", uri);
+			response.setStatusAndReason(HTTPResponse::HTTP_NOT_FOUND);
+			response.setContentLength(0);
+			response.send();
+			return;
+		}
+
 		std::string content_type;
 		try {
-			content_type = all_mimetypes.at(ext);
+			content_type = all_mimetypes.at(path.getExtension());
 		} catch (std::out_of_range& err) {
 		}
 
-		response.sendFile(path.toString(Poco::Path::PATH_UNIX), content_type);
-		return;
-
-		response.setChunkedTransferEncoding(true);
-		response.setContentType("text/html");
-
-		response.send()
-			<< "<html>"
-			<< "<head><title>Hello</title></head>"
-			<< "<body><h1>Hello from the POCO Web Server</h1></body>"
-			<< "</html>\n";
+		response.sendFile(path_str, content_type);
 	}
 };
 
