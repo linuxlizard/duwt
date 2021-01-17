c876a236100a22809d05fc3d6385e347b41ec496
David Poole <dpoole@cradlepoint.com>
2021-01-17 11:25:00 -0700

    move scripting to duwt.js
    finally figure out how to update the table when the survey callback
    chain completes

--- webapp/duwt.js
+++ webapp/duwt.js
@@ -1,13 +1,30 @@
-//import "soup.json"
 
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+function ieee80211_frequency_to_channel(freq)
+{
+	// math from iw util.c 
+	// https://git.kernel.org/pub/scm/linux/kernel/git/jberg/iw.git
+	/* see 802.11-2007 17.3.8.3.2 and Annex J */
+	if (freq == 2484)
+		return 14;
+	else if (freq < 2484)
+		return (freq - 2407) / 5;
+	else if (freq >= 4910 && freq <= 4980)
+		return (freq - 4000) / 5;
+	else if (freq <= 45000) /* DMG band lower limit */
+		return (freq - 5000) / 5;
+	else if (freq >= 58320 && freq <= 64800)
+		return (freq - 56160) / 2160;
+	else
+		return 0;
+}
 
 class Survey {
 	constructor(url){
 		this.url = url;
 		this.survey = [];
 	}
+
+	length() { return this.survey.length; }
 
 	fetch() {
 		return fetch(this.url)
@@ -25,59 +42,58 @@ class Survey {
 											};
 									});
 							this.survey = new_survey;
+							$("#jsGrid1").jsGrid("loadData");
 							return Promise.resolve(new_survey);
 						  })
 	}
-
 }
 
+var mysurvey = new Survey("/api/survey");
+mysurvey.fetch();
+var survey_grid;
 
-// playground for learning wtf I'm doing
-$(document).ready(function() {
-	console.log("hello, world");
-	console.log("survey=",window.survey);
+$(function () {
+	survey_grid = $("#jsGrid1").jsGrid({
+		height: "auto",
+		width: "100%",
+ 
+		sorting: true,
+		paging: true,
+		autoload: true,
+		filtering: true,
+ 
+		onDataLoaded: function(grid,data) {
+				console.log("onDataLoaded data=",data," grid=",grid);
+				},
 
-	$.each(window.survey, (idx,network) => console.log(network));
-
-	var new_survey = $.map(window.survey, (network,idx) => {
-				return {
-					savebssid:network.bssid,
-					...network,
-					bssid:`<a href=/bssid/${network.bssid}>${network.bssid}</a>`,
-					};
-			});
-
-	var survey = new Survey("/api/survey");
-	var survey_promise = survey.fetch();
-	console.log("promise=",survey_promise);
-	console.log("survey=",survey.survey);
-	var table = $("#table-duwt");
-	var tablebody = $("#table-duwt > tbody");
-
-	var filltable = function(survey) {
-		console.log("survey complete; survey=", survey); 
-		console.log("table=",table);
-		console.log("this=",this);
-		$.each( survey, function(index,value) {
-			console.log(`${value.bssid} ${value.ssid} ${value.freq}`);
-			// BSSID SSID Frequency
-			tablebody.append(
-					"<tr>" + 
-						`<td>${value.bssid}</td>` +
-						`<td>${value.ssid}</td>` + 
-						`<td>${value.freq}</td>` + 
-					"</tr>");
-				});
-	}
-
-	// assuming I'm running from index.html which has a simple table; update
-	// the table
-	survey_promise.then(filltable.bind(tablebody));
-//	survey_promise.then( survey => { 
-//				console.log("survey complete; survey=", survey); 
-//				console.log("table=",table);
-//			} );
-
-})
-
+		controller: { 
+			loadData: function(filter) {
+				console.log("loadData filter=",filter);
+				if (mysurvey.length() > 0) {
+					console.log("use cache");
+					var filtered = $.grep(mysurvey.survey, network => {
+								return (!filter.ssid || network.ssid.indexOf(filter.ssid) > -1)
+									&& (!filter.bssid || network.bssid.indexOf(filter.bssid) > -1);
+							});
+					// save the actual bssid then create a link to a bssid page
+					var new_survey = $.map(filtered, (network,idx) => {
+								return {
+									savebssid: network.bssid,
+									...network,
+									bssid:`<a href=/bssid/${network.bssid}>${network.bssid}</a>`,
+									};
+							});
+					return new_survey;
+				}
+			},
+		},
+ 
+		fields: [
+			{ name: "ssid", type: "text", width: 100 },
+			{ name: "bssid", type: "text", width: 75  },
+			{ name: "freq", type: "number", width: 50 },
+			{ name: "dbm", type: "number", width: 50 },
+		]
+	});
+});
 
