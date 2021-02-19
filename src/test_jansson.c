2c6758896f42aa949626764b96b4eba8d331f483
David Poole <dpoole@cradlepoint.com>
2021-02-19 13:06:44 -0700

    fix a few 32-bit integers

--- src/test_jansson.c
+++ src/test_jansson.c
@@ -19,19 +19,16 @@ int main(void)
 	uint64_t num64;
 
 	// jansson uses signed long long for integers. Will that cause problems?
-
 	num8 = num16 = num32 = num64 = 0;
 
-	json_t *jnum, *jobj;
+	json_t *jnum;
 	char *s;
-
-	jobj = json_pack("{sI}", "num8", num8);
 
 	for (size_t i=0 ; i<8 ; i++) {
 		num8 = 1u<<i;
-		jnum = json_integer(num8);
+		jnum = json_integer((json_int_t)num8);
 		assert(jnum);
-		s = json_dumps(json_pack("{sI}", "num8", num8), 0);
+		s = json_dumps(json_pack("{si}", "num8", num8), 0);
 		assert(s);
 		printf("%"PRIu8 " 0x%"PRIx8 " %d %s\n", num8, num8, (int)num8, s);
 	}
@@ -40,7 +37,7 @@ int main(void)
 		num16 = 1u<<i;
 		jnum = json_integer(num16);
 		assert(jnum);
-		s = json_dumps(json_pack("{sI}", "num16", num16), 0);
+		s = json_dumps(json_pack("{si}", "num16", num16), 0);
 		assert(s);
 		printf("%"PRIu16 " 0x%"PRIx16 " %d %s\n", num16, num16, (int)num16, s);
 	}
@@ -49,7 +46,7 @@ int main(void)
 		num32 = 1ul<<i;
 		jnum = json_integer(num32);
 		assert(jnum);
-		s = json_dumps(json_pack("{sI}", "num32", num32), 0);
+		s = json_dumps(json_pack("{si}", "num32", num32), 0);
 		assert(s);
 		printf("%"PRIu32 " 0x%"PRIx32 " %d %s\n", num32, num32, (int)num32, s);
 	}
