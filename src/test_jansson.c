88c39e10cc80e743e30fecd122bc9cda001426cc
David Poole <dpoole@cradlepoint.com>
2021-02-19 12:56:06 -0700

    add a simple program to learn jannson's limits

---
+++ src/test_jansson.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <inttypes.h>
+#include <assert.h>
+
+#include <jansson.h>
+
+int main(void)
+{
+	uint8_t num8;
+	uint16_t num16;
+	uint32_t num32;
+	uint64_t num64;
+
+	num8 = num16 = num32 = num64 = 0;
+
+	json_t *jnum, *jobj;
+	char *s;
+
+	jobj = json_pack("{sI}", "num8", num8);
+
+	for (size_t i=0 ; i<8 ; i++) {
+		num8 = 1u<<i;
+		jnum = json_integer(num8);
+		assert(jnum);
+		s = json_dumps(json_pack("{sI}", "num8", num8), 0);
+		assert(s);
+		printf("%"PRIu8 " 0x%"PRIx8 " %d %s\n", num8, num8, (int)num8, s);
+	}
+
+	for (size_t i=0 ; i<16 ; i++) {
+		num16 = 1u<<i;
+		jnum = json_integer(num16);
+		assert(jnum);
+		s = json_dumps(json_pack("{sI}", "num16", num16), 0);
+		assert(s);
+		printf("%"PRIu16 " 0x%"PRIx16 " %d %s\n", num16, num16, (int)num16, s);
+	}
+
+	for (size_t i=0 ; i<32 ; i++) {
+		num32 = 1ul<<i;
+		jnum = json_integer(num32);
+		assert(jnum);
+		s = json_dumps(json_pack("{sI}", "num32", num32), 0);
+		assert(s);
+		printf("%"PRIu32 " 0x%"PRIx32 " %d %s\n", num32, num32, (int)num32, s);
+	}
+
+	num64 = 0ull;
+	for (size_t i=0 ; i<64 ; i++) {
+		num64 = 1ull<<i;
+		jnum = json_integer(num64);
+		assert(jnum);
+		s = json_dumps(json_pack("{sI}", "num64", num64), 0);
+		assert(s);
+		printf("%"PRIu64 " 0x%"PRIx64 " %d %s\n", num64, num64, (int)num64, s);
+	}
+
+	return 0;
+}
+
