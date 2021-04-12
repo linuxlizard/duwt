60ff3ebdf558a591d7fd7a882acb8a97a5cd17ce
David Poole <dpoole@cradlepoint.com>
2021-04-12 17:08:24 -0600

    add a small program to get ouis from stdin
    - add executable oui
    - add run_oui.cc
    
    Reads mac addresses from stdin,  takes first 6 fields, then looks up in
    oui db.

---
+++ src/run_oui.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+#include "oui.h"
+
+int main(int argc, char *argv[])
+{
+	std::string path = "/usr/share/hwdata/oui.txt";
+	ieeeoui::OUI_MA oui { path };
+
+	std::string arg;
+	while (true) {
+//		std::string arg { argv[i] };
+		std::getline(std::cin, arg);
+		if (arg.length() == 0) {
+			break;
+		}
+
+		std::string s = arg.substr(0,8);
+		s.erase(2,1);
+		s.erase(4,1);
+
+		// https://stackoverflow.com/questions/7131858/stdtransform-and-toupper-no-matching-function
+		std::string findme;
+		std::transform(
+		   s.begin(),
+		   s.end(),
+		   std::back_inserter(findme),
+		   ::toupper                  // global scope
+		);
+		std::cout << "findme=" << findme << "\n";
+
+		uint32_t num32 = ieeeoui::string_to_oui(findme);
+		std::string org = oui.get_org_name(num32);
+		std::cout << "org=" << org << "\n";
+	}
+
+	return 0;
+}
